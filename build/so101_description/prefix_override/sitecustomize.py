2d19a898aa41ecb327612f4275f9202e9a2cb729
Hardbotics <robotics7202@gmail.com>
2026-06-24 17:46:22 +0530

    Stop tracking generated ROS build artifacts

--- build/so101_description/prefix_override/sitecustomize.py
+++
@@ -1,4 +0,0 @@
-import sys
-if sys.prefix == '/usr':
-    sys.real_prefix = sys.prefix
-    sys.prefix = sys.exec_prefix = '/home/mohit-devani/ros2_ws/src/so101_description/install/so101_description'
